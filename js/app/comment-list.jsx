aa6efc9d5e1ec193dded7e24bdf7af2be830856b
Joel Dalley <joeldalley@gmail.com>
2014-11-29 13:14:01 -0700

    Change loop to map.

--- js/app/comment-list.jsx
+++ js/app/comment-list.jsx
@@ -2,15 +2,14 @@ define(['react', 'jsx!app/comment'],
 function(React, Comment) {
     return React.createClass({
         render: function() {
-            var comments = [];
-            for (var i = 0; i < this.props.data.length; i++) {
-                comment = this.props.data[i];
-                comments.push(
-                    <Comment key={'comment-' + i} author={comment.author}>
+            var comments = this.props.data.map(function (comment, idx) {
+                return (
+                    <Comment key={'comment-' + idx} 
+                             author={comment.author}>
                       {comment.text}
                     </Comment>
                 );
-            }
+            });
 
             return (
                 <div className="commentList">
